a9d10e53ec99693ed995a740b915596d2c21a404
MuhammadAzeemAhsan <130462102+MuhammadAzeemAhsan@users.noreply.github.com>
2023-04-20 04:36:35 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-let btn=document.querySelectorAll('.btn')
-let display=document.querySelector('.inpt')
-display.value="";
+let btn = document.querySelectorAll(".btn");
+let display = document.querySelector(".inpt");
+display.value = "";
 
-console.log(display)
-
+console.log(display);
 
 btn.forEach((item) => {
-    item.onclick =()=>{
-    if(item.innerHTML=="AC"){
-        display.value="";
+  item.onclick = () => {
+    if (item.innerHTML == "AC") {
+      display.value = "";
+    } else if (item.innerHTML == "DE") {
+      let str = "" + display.value;
+      display.value = str.slice(0, str.length - 1);
+    } else if (display.value != "" && item.innerHTML == "=") {
+      display.value = eval(display.value);
+    } else if (
+      display.value == "" &&
+      (item.innerHTML == "+" ||
+        item.innerHTML == "-" ||
+        item.innerHTML == "*" ||
+        item.innerHTML == "/" ||
+        item.innerHTML == "=")
+    ) {
+      display.value = "";
+    } else {
+      display.value += item.innerHTML;
     }
-    else if(item.innerHTML=="DE"){
-        let str=""+display.value;
-        display.value=str.slice(0,str.length-1);
-    }
-    else if(display.value!="" && item.innerHTML=="="){
-        display.value=eval(display.value);
-    }
-    else{
-        display.value+=item.innerHTML;
-    }
-
-    }
-})
+  };
+});
