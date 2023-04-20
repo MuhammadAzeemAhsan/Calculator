a0cb85dac48d55e40d31836c68b4d19d68589741
MuhammadAzeemAhsan <130462102+MuhammadAzeemAhsan@users.noreply.github.com>
2023-04-20 04:28:21 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+let btn=document.querySelectorAll('.btn')
+let display=document.querySelector('.inpt')
+display.value="";
+
+console.log(display)
+
+
+btn.forEach((item) => {
+    item.onclick =()=>{
+    if(item.innerHTML=="AC"){
+        display.value="";
+    }
+    else if(item.innerHTML=="DE"){
+        let str=""+display.value;
+        display.value=str.slice(0,str.length-1);
+    }
+    else if(display.value!="" && item.innerHTML=="="){
+        display.value=eval(display.value);
+    }
+    else{
+        display.value+=item.innerHTML;
+    }
+
+    }
+})
